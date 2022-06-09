826cefcfa45d188b86e67845d04d137a97090db5
Ben <brbryant2639@gmail.com>
2022-06-09 14:19:24 -0500

    UPDATE: removed functions comments

--- walkthroughs/javascript/functions/intro.js
+++ walkthroughs/javascript/functions/intro.js
@@ -1,115 +1,117 @@
-// //  Functions
-// let num = 1;
+//  Functions
+let num = 1;
 
-// function greet(person) {
-//   console.log(`Hello, ${person}!`);
-// }
+function greet(person) {
+  console.log(`Hello, ${person}!`);
+}
 
-// // invoke/call a function
-// greet("Cameron");
-// greet("Seth");
-// greet("World");
+// invoke/call a function
+greet("Cameron");
+greet("Seth");
+greet("World");
 
-// // Add Example
-// function add(x = 0, y = 0) {
-//   return x + y;
-// }
+// Add Example
+function add(x = 0, y = 0) {
+  return x + y;
+}
 
-// let sum = add(1, 1, 2, 2); // 2
+let sum = add(1, 1, 2, 2); // 2
 
-// console.log(sum);
+console.log(sum);
 
-// // Sentence Builder
-// function sentenceBuilder(subject, verb, object, noun) {
-//   return `${subject} ${verb} with ${object} and ${noun}.`;
-// }
+// Sentence Builder
+function sentenceBuilder(subject, verb, object, noun) {
+  return `${subject} ${verb} with ${object} and ${noun}.`;
+}
 
-// let sentence = sentenceBuilder("Seth", "travelled", "laptop", "Goku");
-// let paragraph =
-//   sentence + " " + sentenceBuilder("Cameron", "played", "orange", "tree");
+let sentence = sentenceBuilder("Seth", "travelled", "laptop", "Goku");
+let paragraph =
+  sentence + " " + sentenceBuilder("Cameron", "played", "orange", "tree");
 
-// console.log(paragraph);
+console.log(paragraph);
 
-// // Pure vs Side Effect
+// Pure vs Side Effect
 
-// // Pure
-// add(1, 1); // 2
-// add(2, 2); // 4
-// add(1, 1); // 2
-// add(2, 2); // 4
+// Pure
+add(1, 1); // 2
+add(2, 2); // 4
+add(1, 1); // 2
+add(2, 2); // 4
 
-// // Side Effect
-// greet("Seth"); // undefined
-// greet("Ben"); // undefined
+// Side Effect
+greet("Seth"); // undefined
+greet("Ben"); // undefined
 
-// let x = 2;
+let x = 2;
 
-// function multiply(y) {
-//   let prod;
-//   if (y) {
-//     prod = num * y;
-//   }
-//   return prod; // throws reference error. prod is not defined
-// }
+function multiply(y) {
+  let prod;
+  if (y) {
+    prod = num * y;
+  }
+  return prod; // throws reference error. prod is not defined
+}
 
-// multiply(10); // 20
+multiply(10); // 20
 
-// console.log(prod); // throws reference error. prod is not defined
+console.log(prod); // throws reference error. prod is not defined
 
-// multiply(12); // 24
+multiply(12); // 24
 
-// x = 4;
+x = 4;
 
-// multiply(10); // 40 - different than before
+multiply(10); // 40 - different than before
 
-// // Local Scopes
-// // 1. Function Scope
-// // 2. Block Scope
+// Local Scopes
+// 1. Function Scope
+// 2. Block Scope
 
-// function greet2(name) {
-//   // determineGreeting hoisted to HERE
-//   let greeting = determineGreeting();
-//   console.log(`${greeting}, ${name}!`);
+function greet2(name) {
+  // determineGreeting hoisted to HERE
+  let greeting = determineGreeting();
+  console.log(`${greeting}, ${name}!`);
 
-//   function determineGreeting() {
-//     // has access to locally scoped name parameter
-//     if (name.length > 4) {
-//       return "Hello";
-//     } else {
-//       return "Hi";
-//     }
-//   }
-// }
+  function determineGreeting() {
+    // has access to locally scoped name parameter
+    if (name.length > 4) {
+      return "Hello";
+    } else {
+      return "Hi";
+    }
+  }
+}
 
-// // Closure
-// function counter() {
-//   let count = 0;
-//   return function increment() {
-//     // Increment is the inner scope
-//     // counter is the outer scope
-//     // Increment's scope encloses the variable count
-//     // and can access if after counter is executed
-//     count++;
-//     return count;
-//   };
-// }
+// Closure
+function counter() {
+  let count = 0;
+  return function increment() {
+    // Increment is the inner scope
+    // counter is the outer scope
+    // Increment's scope encloses the variable count
+    // and can access if after counter is executed
+    count++;
+    return count;
+  };
+}
 
-// let increment = counter();
+let increment = counter();
 
-// increment(); // 1
-// increment(); // 2
-// increment(); // 3
+increment(); // 1
+increment(); // 2
+increment(); // 3
 
-// let increment2 = counter();
+let increment2 = counter();
 
-// increment2(); // 1
-// increment(); // 4
+increment2(); // 1
+increment(); // 4
 
 // Recursion
 
-// for (let i = 0; i < 10; i++) {
-//   console.log(i);
-// }
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// same as 
 
 function countUp(n) {
   if (n > 0) {
