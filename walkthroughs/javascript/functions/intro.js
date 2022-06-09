9ffeaa0fef3717e66a8576e09d7bdb462317640a
Ben <brbryant2639@gmail.com>
2022-06-09 14:18:22 -0500

    UPDATE: added functions walkthrough p2

--- walkthroughs/javascript/functions/intro.js
+++ walkthroughs/javascript/functions/intro.js
@@ -1,82 +1,140 @@
-//  Functions
-let num = 1;
+// //  Functions
+// let num = 1;
 
-function greet(person) {
-  console.log(`Hello, ${person}!`);
-}
+// function greet(person) {
+//   console.log(`Hello, ${person}!`);
+// }
 
-// invoke/call a function
-greet("Cameron");
-greet("Seth");
-greet("World");
+// // invoke/call a function
+// greet("Cameron");
+// greet("Seth");
+// greet("World");
 
-// Add Example
-function add(x = 0, y = 0) {
-  return x + y;
-}
+// // Add Example
+// function add(x = 0, y = 0) {
+//   return x + y;
+// }
 
-let sum = add(1, 1, 2, 2); // 2
+// let sum = add(1, 1, 2, 2); // 2
 
-console.log(sum);
+// console.log(sum);
 
-// Sentence Builder
-function sentenceBuilder(subject, verb, object, noun) {
-  return `${subject} ${verb} with ${object} and ${noun}.`;
-}
+// // Sentence Builder
+// function sentenceBuilder(subject, verb, object, noun) {
+//   return `${subject} ${verb} with ${object} and ${noun}.`;
+// }
 
-let sentence = sentenceBuilder("Seth", "travelled", "laptop", "Goku");
-let paragraph =
-  sentence + " " + sentenceBuilder("Cameron", "played", "orange", "tree");
+// let sentence = sentenceBuilder("Seth", "travelled", "laptop", "Goku");
+// let paragraph =
+//   sentence + " " + sentenceBuilder("Cameron", "played", "orange", "tree");
 
-console.log(paragraph);
+// console.log(paragraph);
 
-// Pure vs Side Effect
+// // Pure vs Side Effect
 
-// Pure
-add(1, 1); // 2
-add(2, 2); // 4
-add(1, 1); // 2
-add(2, 2); // 4
+// // Pure
+// add(1, 1); // 2
+// add(2, 2); // 4
+// add(1, 1); // 2
+// add(2, 2); // 4
 
-// Side Effect
-greet("Seth"); // undefined
-greet("Ben"); // undefined
+// // Side Effect
+// greet("Seth"); // undefined
+// greet("Ben"); // undefined
 
-let x = 2;
+// let x = 2;
 
-function multiply(y) {
-  let prod;
-  if (y) {
-    prod = num * y;
+// function multiply(y) {
+//   let prod;
+//   if (y) {
+//     prod = num * y;
+//   }
+//   return prod; // throws reference error. prod is not defined
+// }
+
+// multiply(10); // 20
+
+// console.log(prod); // throws reference error. prod is not defined
+
+// multiply(12); // 24
+
+// x = 4;
+
+// multiply(10); // 40 - different than before
+
+// // Local Scopes
+// // 1. Function Scope
+// // 2. Block Scope
+
+// function greet2(name) {
+//   // determineGreeting hoisted to HERE
+//   let greeting = determineGreeting();
+//   console.log(`${greeting}, ${name}!`);
+
+//   function determineGreeting() {
+//     // has access to locally scoped name parameter
+//     if (name.length > 4) {
+//       return "Hello";
+//     } else {
+//       return "Hi";
+//     }
+//   }
+// }
+
+// // Closure
+// function counter() {
+//   let count = 0;
+//   return function increment() {
+//     // Increment is the inner scope
+//     // counter is the outer scope
+//     // Increment's scope encloses the variable count
+//     // and can access if after counter is executed
+//     count++;
+//     return count;
+//   };
+// }
+
+// let increment = counter();
+
+// increment(); // 1
+// increment(); // 2
+// increment(); // 3
+
+// let increment2 = counter();
+
+// increment2(); // 1
+// increment(); // 4
+
+// Recursion
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+function countUp(n) {
+  if (n > 0) {
+    countUp(n - 1);
   }
-  return prod; // throws reference error. prod is not defined
+  console.log(n);
 }
 
-multiply(10); // 20
+countUp(10);
 
-console.log(prod); // throws reference error. prod is not defined
+// Arrow Notation
+// written with a lambda expression, or "fat arrow"
 
-multiply(12); // 24
+// function subtract(x, y) {
+//     return x - y
+// }
 
-x = 4;
+const subtract = (x, y) => x - y; // implicit return
 
-multiply(10); // 40 - different than before
+// function sayHi(name) {
+//     return `Hello, ${name}!`
+// }
 
-// Local Scopes
-// 1. Function Scope
-// 2. Block Scope
+const sayHi = (name) => `Hello, ${name}!`; // implicit return with a single parameter (does not require parentheses)
 
-function greet2(name) {
-  // determineGreeting hoisted to HERE
-  let greeting = determineGreeting();
-  console.log(`${greeting}, ${name}!`);
-
-  function determineGreeting() {
-    // has access to locally scoped name parameter
-    if (name.length > 4) {
-      return "Hello";
-    } else {
-      return "Hi";
-    }
-  }
-}
+const isEven = (n) => {
+  return n % 2 == 0;
+};
