d45e941faf94715bd494a71fe4e1d090a5a29d37
Ben <brbryant2639@gmail.com>
2022-06-22 14:05:16 -0500

    UPDATE: added another example of fetch api with codewars users

--- walkthroughs/javascript/async/fetching-data.js
+++ walkthroughs/javascript/async/fetching-data.js
@@ -1,27 +1,96 @@
-const APP_ID = "54f0MhTKuI2nxwD9nhEvN4ifNwephcpz";
-const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const CW_URL = "https://www.codewars.com/api/v1/users";
 
 let form = document.querySelector("form");
-let input = document.querySelector("input");
-let gif = document.querySelector("#gif");
+let input = document.querySelector("#username");
+let main = document.querySelector("main");
 
-form.addEventListener("submit", (event) => {
+// Query Parameter: url?name=value
+// URL Parameter: url/value
+
+// fetch(CW_URL + "/" + username)
+//   .then((res) => res.json())
+//   .then((body) => console.log(body))
+//   .catch((err) => console.error(err));
+
+// function generateElement(tagname, children, options) {
+//   if (tagname == undefined)
+//     throw new Error("Must provide a valid tagname to create an element.");
+
+//   let element = document.createElement(tagname);
+
+//   for (let prop in options) {
+//     element[prop] = options[prop];
+//   }
+
+//   if (children) {
+//     element.append(...children);
+//   }
+
+//   return element;
+// }
+
+// function addResultToDom(user) {
+//   let prevCard = document.querySelector("#userDisplay");
+
+//   if (prevCard) main.removeChild(prevCard);
+
+//   let title = generateElement("h2", null, { textContent: user.username });
+
+//   let listItems = [
+//     generateElement("li", null, { textContent: user.name }),
+//     generateElement("li", null, { textContent: user.honor }),
+//     generateElement("li", null, { textContent: user.ranks.overall.name }),
+//   ];
+
+//   let ul = generateElement("ul", listItems);
+//   let cardBody = generateElement("div", [title, ul], {
+//     className: "card-body",
+//   });
+//   let card = generateElement("div", [cardBody], {
+//     id: "userDisplay",
+//     className: "card",
+//   });
+
+//   main.appendChild(card);
+// }
+
+function addResultToDom(username, name, honor, rank) {
+  let title = document.querySelector("#title");
+  title.textContent = username;
+
+  let list = document.querySelector("#user-attributes");
+
+  let map = [name, honor, rank];
+
+  console.log(list.children);
+
+  for (let i in list.children) {
+    list.children[i].textContent = map[i];
+  }
+
+  let card = document.querySelector(".card");
+  if (card.hidden) card.hidden = false;
+}
+
+async function searchUser(event) {
   event.preventDefault();
+  let username = input.value;
 
-  let search = input.value;
+  try {
+    let res = await fetch(CW_URL + "/" + username);
+    let body = await res.json();
 
-  fetch(`${GIPHY_URL}?s=${search}&api_key=${APP_ID}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((result) => {
-      console.log(result);
-      gif.src = result.data.images.original.url;
-      gif.alt = result.data.title;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    addResultToDom(
+      body.username,
+      body.name,
+      body.honor,
+      body.ranks.overall.name
+    );
 
-  input.value = "";
-});
+    input.value = "";
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+form.addEventListener("submit", searchUser);
